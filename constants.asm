12cc8aeb99a74f6917dab1a31db4ad59dd38b0f6
amemorystore <76083273+amemorystore@users.noreply.github.com>
2022-10-10 16:25:09 -0400

    .

--- constants.asm
+++ constants.asm
@@ -1,9 +1,10 @@
 ; -. _
 INCLUDE "hram.asm"
 INCLUDE "vram.asm"
-
+;.
 INCLUDE "constants/hardware_constants.asm"
 INCLUDE "constants/joy_constants.asm"
 INCLUDE "constants/movement_constants.asm"
 INCLUDE "constants/music_constants.asm"
 INCLUDE "constants/screen_constants.asm"
+;. end
--- hram.asm
+++ hram.asm
@@ -1,6 +1,10 @@
+; -. _
 ; can't use $ff80 - $ff89 because
 ; that's where the DMA routine is
+;.
 hJoypadState      EQU $FF90  
 ; see joy_constants.asm
+;.
 hCurrentROMBank   EQU $FFF8
 hNextROMBank      EQU $FFF9
+;. end
